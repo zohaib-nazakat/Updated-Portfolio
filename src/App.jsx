afbf7c93052ef0d427457dfd806bcb3616428d09
zohaib-nazakat <zohaibnazakat@gmail.com>
2023-09-08 22:52:28 -0700

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,20 @@ import Skills from './Components/Skills/Skills';
 import Aboutme from './Components/About/About';
 import Technology from './Components/Technology/Technology';
 import Contact from './Components/Contact/Contact';
+import Footer from './Footer/Footer';
 
 function App() {
 
   return (
    <>
-    <Header/>
+    {/* <Header/>
     <Home/>
     <Skills/>
     <Aboutme/>
     <Technology/>
-    <Contact/>
+    <Contact/> */}
+
+    <Footer/>
     
    </>
   )
---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Footer.css';
+import { Link } from 'react-scroll';
+import { CopyAll, Copyright, Facebook, GitHub, LinkedIn, Twitter } from '@mui/icons-material';
+
+const Footer = () => {
+  return (
+        <>
+            <section id="footer">
+                
+                <div className="footerTitle">Front-End Developer</div>
+                <div className="footerText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore corporis eaque consequuntur ipsam nobis, officia aliquid consequatur. Temporibus, aspernatur? Quos.</div>
+
+
+                    <div className="footerIons">
+                        <span className='fIcons'><Twitter/></span>
+                        <span className='fIcons'><Facebook/></span>
+                        <span className='fIcons'><GitHub/></span>
+                        <span className='fIcons'><LinkedIn/></span>
+                </div>
+                <hr />
+                <div className="footerBottom">
+                    <h3><Copyright/> | Front-End Developer</h3>
+                </div>
+                
+            </section>
+        </>
+    )
+}
+
+export default Footer
